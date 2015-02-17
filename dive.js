9e5437aa479d561af9b767363f64f0f16e36880f
tssajo <tssajo@gmail.com>
2015-02-17 12:14:33 +0100

    Fixed issue: complete function did not always get called when an error occured during reading the directory tree

--- dive.js
+++ dive.js
@@ -21,74 +21,78 @@ function defaults(defaults, obj) {
 module.exports = function(dir, opt, action, complete) {
 
   // check args
-  if (typeof opt == 'function') {
-    if (typeof action == 'undefined')
+  if (typeof opt === 'function') {
+    if (typeof action === 'undefined')
       complete = function () {};
     else
       complete = action;
 
     action = opt;
     opt = { };
-  } else if (typeof complete == 'undefined')
+  } else if (typeof complete === 'undefined')
     complete = function () {};
 
   // Assert that dir is a string
-  if (typeof dir != 'string')
+  if (typeof dir !== 'string')
     dir = process.cwd();
 
   opt = defaults(defaultOpt, opt);
 
   function dive(dir) {
     // Read the directory
+    todo++;
     fs.readdir(dir, function(err, list) {
       todo--;
       // Return the error if something went wrong
-      if (err) return action(err);
+      if (err) {
+        action(err, dir);
+      } else {
+        // For every file in the list
+        list.forEach(function(file) {
 
-      // For every file in the list
-      list.forEach(function(file) {
+          if (opt.all || file[0] !== '.') {
 
-        if (opt.all || file[0] != '.') {
-          todo++;
-
-          // Full path of that file
-          var fullPath = path.resolve(dir, file);
-          // Get the file's stats
-          fs.stat(fullPath, function(err, stat) {
-            if (err) {
+            // Full path of that file
+            var fullPath = path.resolve(dir, file);
+            // Get the file's stats
+            todo++;
+            fs.stat(fullPath, function(err, stat) {
               todo--;
-              return action(err);
-            }
-
-            // If the file is a directory
-            if (stat) {
-              if (stat.isDirectory()) {
-                // Call action if enabled for directories
-                if (opt.directories)
-                  action(null, fullPath);
-
-                // Dive into the directory
-                if (opt.recursive)
-                  dive(fullPath);
+              if (err) {
+                action(err, fullPath);
               } else {
-                // Call action if enabled for files
-                if (opt.files)
-                  action(null, fullPath);
+                if (stat) {
+                  // If the file is a directory
+                  if (stat.isDirectory()) {
+                    // Call action if enabled for directories
+                    if (opt.directories)
+                      action(null, fullPath);
 
-                if (!--todo)
-                  complete();
+                    // Dive into the directory
+                    if (opt.recursive)
+                      dive(fullPath);
+
+                  } else {
+                    // Call action if enabled for files
+                    if (opt.files)
+                      action(null, fullPath);
+                  }
+                }
               }
-            }
-          });
-        }
-      });
-      //empty directories, or with just hidden files
-      if (!todo){
-        complete();
-      }
-    });
-  };
+              if (!todo)
+                complete();
+            });
+          }
 
-  var todo = 1;
-  dive(dir);
+        });
+
+      }
+      //empty directories, or with just hidden files
+      if (!todo)
+        complete();
+    });
+  }
+
+  var todo = 0;
+  dive(path.resolve(dir));
 };
